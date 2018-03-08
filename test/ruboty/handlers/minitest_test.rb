a864c499a341a270ae62c9944e2b95c1fc9f94a2
zeero <zeero26@gmail.com>
2018-03-08 23:50:35 +0900

    test hit or not with mock

--- test/ruboty/handlers/minitest_test.rb
+++ test/ruboty/handlers/minitest_test.rb
@@ -6,7 +6,13 @@ describe Ruboty::Handlers::Minitest do
 
   describe '#hello' do
     it 'Actionのcallが呼ばれることの検証' do
-      assert true
+      Ruboty::Minitest::Actions::Hello.any_instance.expects(:call).once
+      Ruboty::Robot.new.receive(body: 'ruboty minitest hello', from: 'sender', to: 'channel')
+    end
+
+    it 'Actionのcallが呼ばれないことの検証' do
+      Ruboty::Minitest::Actions::Hello.any_instance.expects(:call).never
+      Ruboty::Robot.new.receive(body: 'ruboty minitest never', from: 'sender', to: 'channel')
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require 'mocha'
+require 'mocha/mini_test'
 
 require 'ruboty'
 
