0bab66613aafddbd2fddae0684abe27219298ce0
zeero <zeero26@gmail.com>
2018-03-09 05:09:55 +0900

    正規表現の検証を追加

--- lib/ruboty/handlers/minitest.rb
+++ lib/ruboty/handlers/minitest.rb
@@ -3,7 +3,7 @@ require "ruboty/minitest/actions/hello"
 module Ruboty
   module Handlers
     class Minitest < Base
-      on /minitest hello/, name: 'hello', description: 'TODO: write your description'
+      on /minitest (?<hello>hello)/, name: 'hello', description: 'TODO: write your description'
 
       def hello(message)
         Ruboty::Minitest::Actions::Hello.new(message).call
--- test/ruboty/handlers/minitest_test.rb
+++ test/ruboty/handlers/minitest_test.rb
@@ -6,7 +6,16 @@ describe Ruboty::Handlers::Minitest do
 
   describe '#hello' do
     it 'Actionのcallが呼ばれることの検証' do
-      Ruboty::Minitest::Actions::Hello.any_instance.expects(:call).once
+      # Action.newをstubにするため、Action#callはmockにする（一回呼ばれているかは検証する）
+      mock = mock()
+      mock.expects(:call).once
+
+      # Action.newの引数のmessageを取り出して、正規表現のマッチが正しいかを検証する
+      Ruboty::Minitest::Actions::Hello
+        .stubs(:new)
+        .with { |message| message[:hello].must_equal 'hello' }
+        .returns(mock)
+
       Ruboty::Robot.new.receive(body: 'ruboty minitest hello', from: 'sender', to: 'channel')
     end
 
